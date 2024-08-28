de499bd24e0cf13916fa87f8b55d211b3f5378a8
Arthur Miguel <Amsbsimoes@gmail.com>
2024-08-28 19:50:42 -0300

    secund comment

---
+++ demo/src/test/java/com/xadres_Java/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xadres_Java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
